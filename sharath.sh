5ad2a0413b88e028934082a7810419ea7eeb5e61
sharath shepur <sharathshepur@gmail.com>
2024-09-30 22:22:57 +0530

    2nd updated file

---
+++ sharath.sh
@@ -0,0 +1,7 @@
+General Git & GitHub Questions
+
+1) What is Git, and how does it differ from GitHub?
+2) Explain the Git branching model. Why is it useful?
+3) What is the purpose of a pull request?
+4) How do you resolve a merge conflict?
+5) What is the difference between git merge and git rebase?
